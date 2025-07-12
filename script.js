ba39e379bd1e52448d80546d6c693d215e3c33ae
Nidhish Zala <nidhish1132007@gmail.com>
2025-07-12 19:27:20 +0530

    Added intro animation and fixed layout handling

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let animationDiv = document.querySelector(".anime");
+let anime = document.querySelectorAll(".anime div h2");
+let main = document.querySelector(".main");
 let cursor = document.querySelector(".cursor");
 let page1Content = document.querySelector(".page1Content");
 let rejouice = document.querySelectorAll(".page1Content h1 span");
@@ -7,21 +10,53 @@ let textContent5 = document.querySelectorAll(".page5 .textContent p span");
 let page2 = document.querySelector(".page2");
 let page3 = document.querySelector(".page3");
 let page5 = document.querySelector(".page5");
-let seatspage = document.querySelector(".seats")
-let nums = document.querySelector('#nums')
+let seatspage = document.querySelector(".seats");
+let nums = document.querySelector("#nums");
 
+function start() {
+  let tl = gsap.timeline();
+
+  document.body.classList.add("noscroll");
+
+  tl.from(anime, {
+    x: 250,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.2,
+  });
+
+  tl.to(anime, {
+    x: -250,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    stagger: 0.2,
+  });
+
+  tl.to(animationDiv, {
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+  });
+  tl.call(() => {
+    document.body.style.height = "auto";
+    document.body.style.overflow = "auto"; // (optional, enables scrolling again)
+  });
+  tl.from(rejouice, {
+    y: 100,
+    stagger: 0.1,
+    opacity: 0,
+    duration: 0.4,
+    ease: "power2.out",
+  });
+}
+
+start();
 
 document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.pageX + "px";
   cursor.style.top = e.pageY + "px";
-});
-
-gsap.from(rejouice, {
-  y: 100,
-  stagger: 0.1,
-  opacity: 0,
-  duration: 0.4,
-  ease: "power2.out",
 });
 
 gsap.from(textContent2, {
@@ -35,7 +70,6 @@ gsap.from(textContent2, {
     start: "top 30%",
     toggleActions: "play none none reverse",
   },
-  
 });
 
 gsap.from(textContent3, {
@@ -50,7 +84,6 @@ gsap.from(textContent3, {
     start: "top 30%",
     toggleActions: "play none none reverse",
   },
-  
 });
 
 gsap.from(textContent5, {
@@ -64,9 +97,7 @@ gsap.from(textContent5, {
     start: "top 30%",
     toggleActions: "play none none reverse",
   },
-  
 });
-
 
 gsap.from(nums, {
   y: -180,
